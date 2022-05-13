33333dca06515c2f80fe58d014ae7f08e448371f
shahriar <shahriar.mehedi@bjitgroup.com>
2022-05-13 15:12:21 +0600

    c password

--- controller/userController.js
+++ controller/userController.js
@@ -23,7 +23,14 @@ class userController{
                             tc:tc
                         })
                         await doc.save();
-                        res.status(201).send({"status":"Succesfully register"})
+                        const saved_user = await UserModel.findOne({email:email})
+
+                        //Generate jwt Token
+
+                        const token = jwt.sign({userID:saved_user._id},
+                            process.env.JWT_SECRET_KEY,{expiresIn:'5d'})
+
+                        return res.status(201).send({"status":"Succesfully register","token":token})
                     } catch (error) {
                         console.log(error);
                         res.send({"status":"Unable to register"})
@@ -47,11 +54,17 @@ class userController{
                 if(user != null){
                     const isMatch = await bcrypt.compare(password,user.password)
                     if((user.email) && isMatch){
-                        res.send({"status":"WELCOME TO DASHBOARD"})
+                        const saved_user = await UserModel.findOne({email:email})
+
+                        //Generate jwt Token
+
+                        const token = jwt.sign({userID:saved_user._id},
+                            process.env.JWT_SECRET_KEY,{expiresIn:'5d'})
+                        res.send({"status":"WELCOME TO DASHBOARD" , "token" : token})
                     }
                     else{
                         res.send({"status":"Password or email doesnt valid"})
-                    }
+                    }  
                 }
                 else{
                     res.send({"status":"You are not registered user"})  
@@ -65,6 +78,18 @@ class userController{
             res.send({"status":"Unable to login"})
         }
     }
+    static changePassword = async(req,res)=>{
+        const {password,password_confirmation} =req.body
+        if(password && password_confirmation){
+            if(password !== password_confirmation){
+                res.send({"status":"Password and confirm password does not match"})
+            }
+        }
+        else{
+            res.send({"status":"All fields are required"})
+        }
+    }
+
 }
 
 export default userController
